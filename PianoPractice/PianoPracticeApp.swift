3c80ab8c53ef3001c123fca37d97355c372d5ccd
James Baxter <james.baxter@focusrite.com>
2021-08-14 08:28:15 +0800

    Initial Commit

---
+++ PianoPractice/PianoPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoPracticeApp.swift
+//  PianoPractice
+//
+//  Created by James Baxter on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PianoPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
