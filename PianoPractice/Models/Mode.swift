a62662dc5fb1d96469c578ba45d69bd022db6c02
James Baxter <james.baxter@focusrite.com>
2021-08-25 15:38:39 +0800

    Add other minor scales

--- PianoPractice/Models/Mode.swift
+++ PianoPractice/Models/Mode.swift
@@ -1,12 +1,16 @@
 enum Mode: String, CaseIterable, Random {
     case major = "Major"
-    case minor = "Minor"
+    case minor = "Natural Minor"
+    case harmonicMinor = "Harmonic Minor"
+    case melodicMinor = "Melodic Minor"
     case chromatic = "Chromatic"
 
     var intervals: [Int] {
         switch self {
         case .major: return [0, 2, 4, 5, 7, 9, 11]
         case .minor: return [0, 2, 3, 5, 7, 8, 10]
+        case .harmonicMinor: return [0, 2, 3, 5, 7, 8, 11]
+        case .melodicMinor: return [0, 2, 3, 5, 7, 9, 11, 0, 10, 8, 7, 5, 3, 2, 0]
         case .chromatic: return Array(0...11)
         }
     }
